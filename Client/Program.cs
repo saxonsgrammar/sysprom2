619d8801baf636c009af7a93460a862c60cdf264
saxonsgrammar <gramatikvlad1986@gmail.com>
2023-12-03 21:36:22 +0200

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port;
+            string adress, message = "";
+
+            do
+            {
+                Console.Write("Введите номер порта: ");
+                port = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите адрес: ");
+                adress = Console.ReadLine();
+
+                if (port != 8005 && adress != "127.0.0.1")
+                {
+                    Console.WriteLine("Неверный номер порта или/и адрес\n");
+                }
+            }
+            while (port != 8005 && adress != "127.0.0.1");
+
+            try
+            {
+                IPEndPoint point = new IPEndPoint(IPAddress.Parse(adress), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(point);
+
+                StringBuilder data = new StringBuilder();
+                Console.WriteLine();
+
+                while (message != "BYE")
+                {
+                    Console.Write("Введите сообщение: ");
+                    message = Console.ReadLine();
+
+                    socket.Send(Encoding.UTF8.GetBytes(message));
+                    byte[] buffer = new byte[256];
+                    data = new StringBuilder();
+
+                    do
+                    {
+                        int size = socket.Receive(buffer);
+                        data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                    }
+                    while (socket.Available > 0);
+
+                    Console.WriteLine("Ответ сервера: " + data.ToString());
+                };
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 8005;
+            const string adress = "127.0.0.1";
+            string[] quotes =
+            {
+                 "Или не берись, или доводи до конца",
+                 "Капля долбит камень не силой, но частым падением",
+                 "Счастлив тот, кто умело берет под свою защиту то, что любит",
+                 "Бесцветные зелёные идеи спят яростно",
+                 "Глокая куздра штеко будланула бокра и кудрячит бокрёнка"
+            };
+            Random random = new Random();
+
+            try
+            {
+                IPEndPoint point = new IPEndPoint(IPAddress.Parse(adress), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Bind(point);
+                socket.Listen(3);
+
+                Console.WriteLine("Сервер успешно запущен...\n");
+                StringBuilder data = new StringBuilder();
+                Socket listener = socket.Accept();
+
+                while (data.ToString() != "BYE")
+                {
+                    byte[] buffer = new byte[256];
+                    data = new StringBuilder();
+
+                    do
+                    {
+                        int size = listener.Receive(buffer);
+                        data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                    }
+                    while (listener.Available > 0);
+
+                    Console.WriteLine("Сообщение клиента: " + data.ToString());
+                    listener.Send(Encoding.UTF8.GetBytes(quotes[random.Next(0, 5)]));
+                };
+
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
